42d2721e95130895d7043a18083f95ca0f925ecf
lautiipereiraa <lautaropereirag@gmail.com>
2023-10-25 16:03:51 -0300

    cree los modelos, seeder y las migrations

--- database/migrations/2023_10_25_185606_create_user_table.php
+++ database/migrations/2023_10_25_185606_create_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nombre',255);
             $table->string('email',255);
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('users');
     }
 };
